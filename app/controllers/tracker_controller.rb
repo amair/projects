d0e6904b90f88e93356d50ca37154b424070ec46
Alastair Mair <alastair@ubuntu.(none)>
2011-09-15 17:43:49 +0100

    Check for invalid token - part 1 (dirty!)

--- app/controllers/tracker_controller.rb
+++ app/controllers/tracker_controller.rb
@@ -17,37 +17,41 @@ class TrackerController < ApplicationController
       rescue => e
         logger.error e.response
       end
-
-      stories = []
-      @all_development = []
-      @all_testing = []
-      @all_completed = []
-
-      @Projects.each do |p| 
-        begin
-          logger.debug "Getting Stories for project #{p.id}"
-          stories = p.stories.all(:story_type => ['bug','chore','feature'])
-          stories = p.stories.all()
-          logger.debug "Retrieved #{stories.length} stories"
-        rescue => e
-          logger.error e.response
-        end
-
-        subset = stories.select{|s| s.current_state == 'started'}
-        logger.debug "There are #{subset.length} started stories"
-        @all_development += subset unless subset.empty?
-
-
-        subset = stories.select{|s| s.current_state == 'delivered'}
-        logger.debug "There are #{subset.length} delivered stories"
-        @all_testing += subset unless subset.empty?
-
-        subset = stories.select{|s| s.current_state == 'accepted'}
-        logger.debug "There are #{subset.length} accepted stories"
-        @all_completed += subset unless subset.empty?
-      end
-      logger.debug "Got all stories!"
+      getStories unless @Projects.nil?
     end
   end
+
+  def getStories
+    stories = []
+    @all_development = []
+    @all_testing = []
+    @all_completed = []
+
+    @Projects.each do |p| 
+      begin
+        logger.debug "Getting Stories for project #{p.id}"
+        stories = p.stories.all(:story_type => ['bug','chore','feature'])
+        stories = p.stories.all()
+        logger.debug "Retrieved #{stories.length} stories"
+      rescue => e
+        logger.error e.response
+      end
+
+      subset = stories.select{|s| s.current_state == 'started'}
+      logger.debug "There are #{subset.length} started stories"
+      @all_development += subset unless subset.empty?
+
+
+      subset = stories.select{|s| s.current_state == 'delivered'}
+      logger.debug "There are #{subset.length} delivered stories"
+      @all_testing += subset unless subset.empty?
+
+      subset = stories.select{|s| s.current_state == 'accepted'}
+      logger.debug "There are #{subset.length} accepted stories"
+      @all_completed += subset unless subset.empty?
+    end
+    logger.debug "Got all stories!"
+  end
+
 
 end
