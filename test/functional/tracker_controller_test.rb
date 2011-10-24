905ab3ec6075602f1c1b8581dcf1ba0484711c49
Alastair Mair <alastair@ubuntu.(none)>
2011-10-24 09:09:07 +0100

    Test framework setup and swap axes for bugs/points

--- test/functional/tracker_controller_test.rb
+++ test/functional/tracker_controller_test.rb
@@ -1,6 +1,12 @@
 require 'test_helper'
 
 class TrackerControllerTest < ActionController::TestCase
+
+    def setup
+    require 'active_resource/http_mock'
+    ##rest of your code below....
+    end
+
   test "should get index" do
     get :index
     assert_response :success
