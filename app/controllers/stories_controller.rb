b2c3f69dfe03a6ee384e7252edea71e0af2584a6
Alastair Mair <alastair@ubuntu.(none)>
2011-10-28 16:17:34 +0100

    Added tasks to single story view

--- app/controllers/stories_controller.rb
+++ app/controllers/stories_controller.rb
@@ -3,9 +3,17 @@ class StoriesController < PivotalManagement
   def index
     logger.debug "Story #{params[:id]}"
     unless params[:id].nil?
-      logger.debug "Retrieve story"
       retrieveProjects
       @story = lookupStory( params[:id] )
+
+      @tasks =  @story.tasks.all
+      #unless @story.tasks.nil? || @story.tasks.empty?
+      #  @story.tasks.each do |task|
+      #    if (!task.complete)
+      #    end
+      #  end
+      #end
+
     end
   end
 end
